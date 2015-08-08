c9e53a032c395cd06bd1ed724c222ca4697426b8
Peter Hellberg <peter@c7.se>
2015-08-08 17:53:17 +0200

    Updated VisitDir test

--- listing_test.go
+++ listing_test.go
@@ -30,21 +30,38 @@ func TestListingString(t *testing.T) {
 }
 
 func TestListingVisitDir(t *testing.T) {
-	l := Listing{[]Entry{
-		{'0', "Foo", "foo", "test", "70"},
-		{'1', "Bar", "bar", "test", "70"},
-	}}
+	for _, tt := range []struct {
+		listing Listing
+		count   int
+		want    string
+		name    string
+		path    string
+		root    string
+		host    string
+		port    string
+	}{
+		{Listing{}, 1, "\x00\t\t\t\r\n", "", "", "", "", ""},
+		{Listing{[]Entry{
+			{'0', "Foo", "foo", "test", "70"},
+			{'1', "Bar", "bar", "test", "70"},
+		}}, 3, "1Dir\tbaz\texample\t70\r\n",
+			"Dir", "bar/baz", "bar/", "example", "70"},
+	} {
+		l := tt.listing
 
-	l.VisitDir("Dir", "bar/baz", "bar/", "example", "70")
+		l.VisitDir("Dir", "bar/baz", "bar/", "example", "70")
 
-	if got, want := len(l.entries), 3; got != want {
-		t.Fatalf(`len(l.entries) = %d, want %d`, got, want)
-	}
+		if got, want := len(l.entries), tt.count; got != want {
+			t.Fatalf(`len(l.entries) = %d, want %d`, got, want)
+		}
 
-	e := l.entries[2]
+		if tt.count > 0 {
+			e := l.entries[tt.count-1]
 
-	if got, want := e.String(), "1Dir\tbaz\texample\t70\r\n"; got != want {
-		t.Fatalf(`e.String() = %q, want %q`, got, want)
+			if got := e.String(); got != tt.want {
+				t.Fatalf(`e.String() = %q, want %q`, got, tt.want)
+			}
+		}
 	}
 }
 
