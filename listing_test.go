e5683d17e2ccb403e9cc627ca9359eca8512a643
Peter Hellberg <peter@c7.se>
2018-04-11 18:00:30 +0200

    Add a few more file types

--- listing_test.go
+++ listing_test.go
@@ -3,7 +3,7 @@ package gopher
 import "testing"
 
 func TestSuffixes(t *testing.T) {
-	if got, want := len(Suffixes), 17; got != want {
+	if got, want := len(Suffixes), 34; got != want {
 		t.Fatalf(`len(Suffixes) = %d, want %d`, got, want)
 	}
 }
